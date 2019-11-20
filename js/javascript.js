f62c0a22465da6589eb4336ce5db8e0df3ae64b9
Gwen <itsgwenhey@gmail.com>
2019-11-19 20:02:43 -0600

    Finish name box and nav
    finish name box : styling and text, as well as finish the changing word bit.
    
    finish nav: add items, style, and positioning.

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,25 @@
+let topPAnim = document.getElementById('topPChange');
 
+let count = 0;
 
+document.addEventListener('DOMContentLoaded', () => {
+    topPAnim.style.animation = '2500ms linear infinite orangeTopFade1';
+    window.setInterval(() => {
+        if(count == 0){
+            topPAnim.innerText = 'Designer';
+            count += 1;
+        } else if(count == 1){
+            topPAnim.innerText = 'Cooperator';
+            count += 1;
+        } else if(count == 2){
+            topPAnim.innerText = 'Student';
+            count += 1;
+        } else if(count == 3) {
+            topPAnim.innerText = 'Nerd'
+            count += 1;
+        } else{
+            topPAnim.innerText = 'Developer';
+            count = 0;
+        }
+    }, 2500);
+});
